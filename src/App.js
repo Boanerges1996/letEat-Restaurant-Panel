06ab96966d77a9d3c02ef8de588d8f7d75f50e39
Boanerges1996 <samsonnkrumah253@gmail.com>
2020-07-15 14:35:03 +0530

    details

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Landing from "./components/landing/landing";
+import Login from "./components/login/login";
+import SignUp from "./components/signup/signup";
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/landing/landing.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+  MenuItem,
+  Typography,
+  MenuI,
+} from "@material-ui/core";
+
+export default class Landing extends Component {
+  render() {
+    return <div></div>;
+  }
+}
