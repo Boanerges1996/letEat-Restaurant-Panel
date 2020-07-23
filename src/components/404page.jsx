c38336c71a02551e4af592efade8dd4d876ef166
Onuoha Collins <onuohacollinschidubem@gmail.com>
2020-07-23 14:09:58 -0700

    done with error page

--- src/components/404page.jsx
+++ src/components/404page.jsx
@@ -1,7 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import img404 from '../img/Error.jpeg'
+import '../error.css'
 
-export default class ErrorPage extends Component {
-  render() {
-    return <div>error</div>;
-  }
+
+class ErrorPage extends Component {
+    render() {
+        return (
+            <div className="container">
+                <img src={img404} width='200px' height='200px' alt="404 image" className="center" />
+                <h2 style={{ textAlign: 'center', color: '#e91e63' }}> Opps it seems your are on a wrong page</h2>
+                <div className='container_btn'>
+                    <div className="vertical_center">
+                        <Link to='/' >
+                            <Button variant='contained' style={{ color: "white", background: '#e91e63', marginRight: 'auto' }} >
+                                go to home page
+                          </Button>
+                        </Link>
+                    </div>
+
+                </div>
+            </div>
+
+
+        );
+    }
 }
+
+export default ErrorPage;
